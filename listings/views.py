58aebad9da3caa2b8bfd769b383c16d62c77e342
Alex Lag <laxeret@gmail.com>
2019-03-11 23:47:22 +0300

    Added search logic

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
-
+from .choices import price_choices, bedroom_choices, state_choices
 
 def index(request):
+    
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
     paginator = Paginator(listings, 6)
@@ -19,8 +20,22 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+    }
+
+    return render(request, 'listings/search.html', context)
